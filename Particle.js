57e5e115890736673245d1823982d1650e793916
Isra08dc <90532141+Isra08dc@users.noreply.github.com>
2022-01-19 18:20:17 -0600

    Add files via upload

--- Particle.js
+++ Particle.js
@@ -7,6 +7,8 @@ class Particle {
         }
         this.r = 10;
         this.body = Bodies.circle(x, y, this.r, options);
+        this.color=color(random(0, 255), random(0, 255), random(0, 255));
+
         
         World.add(world, this.body);
     }
@@ -20,7 +22,7 @@ class Particle {
         rotate(angle);
         imageMode(CENTER);
         noStroke();
-        fill("white");
+        fill(this.color);
         ellipseMode(RADIUS);
         ellipse(0,0,this.r,this.r);
         pop();
